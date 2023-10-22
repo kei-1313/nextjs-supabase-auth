09d78db040356866624186dfef9670d53bb5381b
kei-1313 <candydog666@gmail.com>
2023-10-22 22:43:25 +0900

    login部分途中

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,25 @@
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { redirect } from 'next/navigation'
+import Login from '@/app/components/Login'
+import type { Database } from '@/lib/database.types'
+
+const LoginPage = async () => {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  })
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // 認証している場合、リダイレクト
+  if(session) {
+    redirect('/')
+  }
+
+  return <Login/>
+}
+
+export default LoginPage

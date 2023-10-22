7568f2258526e6fc35a62255b5f26b6e4a5e383d
kei-1313 <candydog666@gmail.com>
2023-10-22 14:11:31 +0900

    初期設定

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex-1 container max-w-screen-sm mx-auto px-1 py-5">
       <h1>HOME</h1>
     </main>
   )

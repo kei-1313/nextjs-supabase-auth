9e476ee830226941a64281eb8606924364caee02
kei-1313 <candydog666@gmail.com>
2023-10-25 22:51:02 +0900

    storeで定義した値をナビゲーションコンポーネントに渡す

--- app/components/SupabaseListener.tsx
+++ app/components/SupabaseListener.tsx
@@ -12,7 +12,31 @@ const SupabaseListener = async () => {
   //セッションの取得
   const { data: { session }} = await supabase.auth.getSession()
 
-  return <Navigation session={session}/>
+  let profile = null
+   
+  if(session) {
+    const {data: currentProfile } = await supabase
+      .from('profiles')
+      .select('*')
+      .eq('id', session.user.id)
+      .single()
+
+      profile = currentProfile
+
+      //メールアドレスを変更した場合、プロフィールを更新
+      if(currentProfile && currentProfile.email !== session.user.email) {
+        const { data: updatedProfile } = await supabase
+          .from('profiles')
+          .update({ email: session.user.email })
+          .match({ id: session.user.id })
+          .select('*')
+          .single()
+
+        profile = updatedProfile
+      }
+  }
+
+  return <Navigation session={session} profile={profile}/>
 }
 
 export default SupabaseListener

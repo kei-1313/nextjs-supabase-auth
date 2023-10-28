801bdcdccf22c05ac279ef33d8027c5a3eea6ea8
kei-1313 <candydog666@gmail.com>
2023-10-28 22:09:12 +0900

    ログアウトを作成

---
+++ app/components/Logout.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { FormEvent, useState } from 'react'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+import Loading from '@/app/loading'
+import type { Database } from '@/lib/database.types'
+
+
+const Logout = () => { 
+  const router = useRouter()
+  const supabase = createClientComponentClient<Database>()
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState('')
+
+  // 送信
+  const onSubmit = async (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setLoading(true)
+   
+    try {
+      const { error: logoutError } = await supabase.auth.signOut()
+      setMessage("ログアウトしました")
+
+      // エラーチェック
+      if (logoutError) {
+        setMessage('エラーが発生しました。' + logoutError.message)
+        return
+      }
+
+      router.push('/')
+    } catch(error) {
+      setMessage('エラーが発生しました。' + error)
+      return
+    } finally {
+      setLoading(false)
+      router.refresh()
+    }
+  }
+
+	return (
+		<div>
+      <div className="text-center mb-5">ログアウトしますか？</div>
+      {/* ログアウトボタン */}
+      <form onSubmit={onSubmit}>
+        <div className="mb-5">
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              type="submit"
+              className="font-bold bg-red-500 hover:brightness-95 w-full rounded-full p-2 text-white text-sm"
+            >
+              ログアウト
+            </button>
+          )}
+        </div>
+      </form>
+
+      {message && <div className="my-5 text-center text-sm text-red-500">{message}</div>}
+    </div>
+	)
+}
+
+export default Logout
+
